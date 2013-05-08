a2867e8088541f587906c8b7bcb3f280d2c95a7d
Walker Flynn <walker.d.flynn@gmail.com>
2013-05-08 10:19:25 -0700

    stack is a queue, woops

--- dataStructures/Stack/src/Stack.js
+++ dataStructures/Stack/src/Stack.js
@@ -1,14 +1,15 @@
 var Stack = function() {
-  var storage/* = ??? */;
+  var storage = [];
   this.length = function() {
     return storage.length;
   };
 
   this.add = function(value) {
-    /*???*/
+    storage.push(value);
   };
 
   this.remove = function() {
-    /*???*/
+    var removed = storage.shift();
+    return removed;
   };
 };
